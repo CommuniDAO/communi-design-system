b46e5509aae27d5a43a217f5426c3b13f594ef56
Julian Clatro <jclatro@yahoo.com>
2023-03-29 15:34:59 -0300

    v 0.0.5

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -14,8 +14,8 @@ export const parameters = {
   darkMode: {
     current: 'light',
     darkClass: 'midnight',
-    dark: { ...themes.dark, appBg: '#000104', brandImage: 'https://imagedelivery.net/uDbEDRBQqhBXrrfuCRrATQ/3d995683-15fd-4ca3-9e0c-aadbe99e7a00/public' },
-    light: { ...themes.normal, appBg: '#F5F4F7', brandImage: 'https://imagedelivery.net/uDbEDRBQqhBXrrfuCRrATQ/3d995683-15fd-4ca3-9e0c-aadbe99e7a00/public' },
+    dark: { ...themes.dark, appBg: '#000104', brandImage: 'https://imagedelivery.net/uDbEDRBQqhBXrrfuCRrATQ/4238215e-569a-472a-89df-f2eb2fc4ae00/public' },
+    light: { ...themes.normal, appBg: '#E7EDFA', brandImage: 'https://imagedelivery.net/uDbEDRBQqhBXrrfuCRrATQ/4238215e-569a-472a-89df-f2eb2fc4ae00/public' },
     classTarget: 'html',
     stylePreview: true
   },
@@ -24,7 +24,7 @@ export const parameters = {
     values: [
       {
         name: 'light',
-        value: '#F5F4F7'
+        value: '#E7EDFA'
       },
       {
         name: 'dark',
