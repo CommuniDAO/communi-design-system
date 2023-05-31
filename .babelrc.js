2e8853f5220d3a536e983f4610104856f738aa0d
Julian Clatro <jclatro@yahoo.com>
2023-05-31 16:00:31 -0300

    wip

---
+++ .babelrc.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["@babel/preset-env", { "modules": false }],
+      "@babel/preset-react",
+      "@babel/preset-typescript"
+    ],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          "alias": {
+            "~/src": "./src",
+            "~/styles": "./styles"
+          }
+        }
+      ],
+      ["transform-inline-environment-variables"]
+    ],
+    env: {
+      production: {
+        plugins: [["inline-dotenv",{
+          path: '.env'
+        }]]
+      },
+    }
+  };
+};
